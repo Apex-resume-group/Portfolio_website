acbd61221a74795bcd7e14d969e9aeaa62d12129
Apex-resume-group <denisefilippellexecutivesearch@gmail.com>
2026-05-13 04:39:12 -0700

    Update Tailwind.config.js

--- Tailwind.config.js
+++ Tailwind.config.js
@@ -2,9 +2,8 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-     "./index.html",
-  "./*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
+  "./index.html",
+  "./**/*.{js,ts,jsx,tsx}",
 ],
   theme: {
     extend: {
