1caf05069c779ee5760e0605580bab414faa5134
Apex-resume-group <denisefilippellexecutivesearch@gmail.com>
2026-05-13 04:15:47 -0700

    Refactor alias paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,20 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./"),
-"../components/ui": path.resolve(__dirname, "./components/ui"),
-"../components/home": path.resolve(__dirname, "./components/home"),
-"../components/layout": path.resolve(__dirname, "./components/layout"),
-"../components/reviews": path.resolve(__dirname, "./components/reviews"),
-"../components/services": path.resolve(__dirname, "./components/services"),
-"../components/shared": path.resolve(__dirname, "./components/shared"),
-"../components/templates": path.resolve(__dirname, "./components/templates"),
-"./Components/Layout/SiteLayout": path.resolve(__dirname, "./components/layout/Sitelayout"),
-"./components/layout": path.resolve(__dirname, "./components/layout"),
+      "../components/ui": path.resolve(__dirname, "./components/ui"),
+      "../components/home": path.resolve(__dirname, "./components/home"),
+      "../components/layout": path.resolve(__dirname, "./components/layout"),
+      "../components/reviews": path.resolve(__dirname, "./components/reviews"),
+      "../components/services": path.resolve(__dirname, "./components/services"),
+      "../components/shared": path.resolve(__dirname, "./components/shared"),
+      "../components/templates": path.resolve(__dirname, "./components/templates"),
+      "./Components/Layout/SiteLayout": path.resolve(__dirname, "./components/layout/Sitelayout"),
+      "./components/layout": path.resolve(__dirname, "./components/layout"),
     },
   },
 })
